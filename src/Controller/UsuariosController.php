e1e9997df36399c4846afcd57827279ceb2bf47b
Vitor-Novaga <vibunospfc@ymail.com>
2025-06-26 21:29:31 -0400

    criando usuarios_buscar

--- src/Controller/UsuariosController.php
+++ src/Controller/UsuariosController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Dto\UsuarioContaDto;
 use App\Dto\UsuarioDto;
 use App\Entity\Conta;
 use App\Entity\Usuario;
+use App\Repository\ContaRepository;
 use App\Repository\UsuarioRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,24 +19,23 @@ final class UsuariosController extends AbstractController
 {
     #[Route('/usuarios', name: 'usuarios_criar', methods: ['POST'])]
     public function criar(
-        #[MapRequestPayload(acceptFormat:'json')]
+        #[MapRequestPayload(acceptFormat: 'json')]
         UsuarioDto $usuarioDto,
 
         EntityManagerInterface $entitymanager,
 
         UsuarioRepository $usuarioRepository
 
-    ): JsonResponse
-    {
+    ): JsonResponse {
         $erros = [];
 
-            if ((!$usuarioDto->getCpf())) {
+        if ((!$usuarioDto->getCpf())) {
             array_push($erros, [
                 'message' => 'CPF é obrigatório'
             ]);
         }
 
-          if (!($usuarioDto->getNome())) {
+        if (!($usuarioDto->getNome())) {
             array_push($erros, [
                 'message' => 'Nome é obrigatório'
             ]);
@@ -69,7 +70,6 @@ final class UsuariosController extends AbstractController
             return $this->json([
                 'message' => 'O CPF informado já está cadastrado'
             ], 409);
-
         }
 
         // criar um objeot da entidade usuario
@@ -87,18 +87,54 @@ final class UsuariosController extends AbstractController
         //instanciar o objeto conta
 
         $conta = new Conta();
-        // $numeroConta = preg_replace('/\D/', '', uniqid());
-        $numeroConta = rand(1,99999);
+        $numeroConta = preg_replace('/\D/', '', uniqid());
+        $numeroConta = rand(1, 99999);
         $conta->setNumero($numeroConta);
         $conta->setSaldo('0');
         $conta->setUsuario($usuario);
 
         //criar registro na tb conta
 
-        $entitymanager ->persist($conta);
-        $entitymanager ->flush();
+        $entitymanager->persist($conta);
+        $entitymanager->flush();
 
-        
-        return $this->json($usuario);
+        // retornar os dados de usuario e conta
+
+        $usuarioContaDto = new UsuarioContaDto();
+        $usuarioContaDto->setId($usuario->getId());
+        $usuarioContaDto->setCpf($usuario->getCpf());
+        $usuarioContaDto->setNome($usuario->getNome());
+        $usuarioContaDto->setEmail($usuario->getEmail());
+        $usuarioContaDto->setTelefone($usuario->getTelefone());
+        $usuarioContaDto->setSaldo($conta->getSaldo());
+        $usuarioContaDto->setNumeroConta($conta->getNumero());
+
+        return $this->json($usuarioContaDto, status: 201);
+    }
+
+    #[Route('/usuarios/{id}', name: 'usuarios_buscar', methods: ['GET'])]
+    public function buscarPorId(
+        int $id,
+        ContaRepository $contaRepository
+    ) {
+        $conta = $contaRepository->findByUsuarioId($id);
+
+        if (! $conta) {
+            return $this->json([
+                'message' => 'Usuário não encontrado'
+            ], status: 404);
+        }
+
+        $usuarioContaDto = new UsuarioContaDto();
+        $usuarioContaDto->setId($conta->getUsuario()->getId());
+        $usuarioContaDto->setCpf($conta->getUsuario()->getCpf());
+        $usuarioContaDto->setNome($conta->getUsuario()->getNome());
+        $usuarioContaDto->setEmail($conta->getUsuario()->getEmail());
+        $usuarioContaDto->setTelefone($conta->getUsuario()->getTelefone());
+        $usuarioContaDto->setSaldo($conta->getSaldo());
+        $usuarioContaDto->setNumeroConta($conta->getNumero());
+
+        return $this->json($usuarioContaDto, status: 201);
+
     }
 }
