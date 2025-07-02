f32cd295d67dd5eb2db8efda33d5cfef6cafaa3d
Vitor-Novaga <vibunospfc@ymail.com>
2025-07-01 21:52:55 -0400

    SAVE

---
+++ src/Controller/TransacoesController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Controller;
+
+use App\Dto\TransacaoRealizarDto;
+use App\Repository\ContaRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/api')]
+final class TransacoesController extends AbstractController
+{
+    #[Route('/transacoes', name: 'transacoes_realizar', methods: ['POST'])]
+    public function realizar(
+        #[MapRequestPayload(acceptFormat:'json')]
+        TransacaoRealizarDto $entrada,
+
+        ContaRepository $contaRepository
+
+    ): JsonResponse 
+    {
+        $erros=[];
+        // validar o DTO de entrada
+        if (!$entrada->getIdUsuarioOrigem()) {
+            array_push($erros, [
+                'message' => 'Conta de origem é obrigatória!'
+            ]);
+        }
+        if (!$entrada->getIdUsuarioDestino()) {
+            array_push($erros, [
+                'message' => 'Conta de destino é obrigatória!'
+            ]);
+        }
+        if (!$entrada->getValor() ) {
+            array_push($erros, [
+                'message'=> 'Valor é obrigatório!'
+            ]);
+        }
+        if ((float)$entrada->getValor() <= 0) {
+            array_push($erros, [
+                'message'=> 'O valor deve ser maior que zero!'
+            ]);
+        }
+        if ($entrada->getIdUsuarioOrigem() === $entrada->getIdUsuarioDestino()){
+            array_push($erros, [
+                'message'=> 'As contas devem ser diferentes'
+            ]);
+        } 
+        if (count($erros) > 0) {
+            return $this->json($erros,422);
+        }
+        // validações de regra de negocio
+        // validar se as contas existem
+        $contaOrigem = $contaRepository->findByUsuarioId($entrada->getIdUsuarioOrigem());
+        if ($contaOrigem) {
+            return $this->json([
+                'message'=> 'Conta de origem não encontrada'
+            ], 404);
+        }
+
+        $contaDestino = $contaRepository->findByUsuarioId($entrada->getIdUsuarioDestino());
+        if (!$contaDestino) {
+            return $this->json([
+                'message'=> 'Conta de destino não encontrada'
+            ], 404);
+
+        }
+        // validar se a origem tem saldo suficiente
+
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/TransacoesController.php',
+        ]);
+    }
+}
